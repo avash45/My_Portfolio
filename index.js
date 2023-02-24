4defbc65e29a573ef363b5034e0b13b4fe0c00b6
avash45 <avaneesh4564@gmail.com>
2023-02-24 08:52:29 +0530

    feb 24 version 3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const path = require('path')
 const PORT = process.env.PORT || 3000
+const process = require("process")
 
 const app = express()
 app.use(express.static(path.join(__dirname, '/views')))
